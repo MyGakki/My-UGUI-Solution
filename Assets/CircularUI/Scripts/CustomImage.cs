7bab4c53137e4cf2e74f34d03f98a301fc545b4a
HopePower <hxq981120@foxmail.com>
2019-09-16 23:18:27 +0800

    圆形UI的制作

---
+++ Assets/CircularUI/Scripts/CustomImage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomImage : Image
+{
+    private PolygonCollider2D _polygon;
+    public PolygonCollider2D Polygon
+    {
+        get
+        {
+            if (_polygon == null)
+                _polygon = GetComponent<PolygonCollider2D>();
+            return _polygon;
+        }
+    }
+
+    public override bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+    {
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, screenPoint, eventCamera, out Vector3 worldPoint);
+        return Polygon.OverlapPoint(worldPoint);
+    }
+}
